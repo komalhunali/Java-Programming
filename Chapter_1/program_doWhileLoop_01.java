04fa5188e5ec128c69dae2cb72d3c9ff4fd24422
komal hunali <mahihunali@gmail.com>
2024-10-06 13:29:34 +0530

    Add files via upload

---
+++ Chapter_1/program_doWhileLoop_01.java
@@ -0,0 +1,9 @@
+public class program_doWhileLoop_01 {
+    public static void main(String args[]){
+        int x = 1;
+        do{
+            System.out.println("infinite loop ");
+        }while(x==1);
+    }
+    
+}
---
+++ Chapter_1/program_doWhileLoop_04.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class program_doWhileLoop_04 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String correctPass = "iLoveCoding";
+        String inputPass;
+
+        do{
+            System.out.print("Enter password: ");
+            inputPass = sc.nextLine();
+        }while(!inputPass.equals(correctPass));
+
+        System.out.print("Welcome \n Access granted..");
+    }
+}
