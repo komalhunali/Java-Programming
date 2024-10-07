9b28f737fff1e5d02a418566af621002bea19622
komal hunali <mahihunali@gmail.com>
2024-10-07 22:54:53 +0530

    Update program_nested_if_else_10.java

--- Chapter_1/program_nested_if_else_10.java
+++ Chapter_1/program_nested_if_else_10.java
@@ -1,9 +1,9 @@
 public class program_nested_if_else_10 {
     public static void main(String args[]){
-        String fName = "Pravin";
-        String sName = "Junne";
-        if(fName == "Pravin"){
-            if(sName == "Junne"){
+        String fName = "Komal";
+        String sName = "Hunali";
+        if(fName == "Komal"){
+            if(sName == "Hunali"){
                 System.out.println(fName+" "+sName);
                 System.out.println("Ty.BCA(Sci) Student");
                 System.out.println("Ncc Cadet");
