5266c297f9f6ed0f5a5df57e2e232648bb0d4733
komal hunali <mahihunali@gmail.com>
2024-10-07 22:50:57 +0530

    Update program_string_10.java

--- Chapter_1/program_string_10.java
+++ Chapter_1/program_string_10.java
@@ -1,6 +1,6 @@
 public class program_string_10{
     public static void main(String args[]){
-        String str1 = "pravin";
+        String str1 = "komal";
         
         for(int i=0; i<str1.length();i++){
             System.out.println(str1.charAt(i)); 
