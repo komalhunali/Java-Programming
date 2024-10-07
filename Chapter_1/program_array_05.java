b7c39e5ca77eec02a4eddacce22e3d2648bf5b36
komal hunali <mahihunali@gmail.com>
2024-10-07 22:54:02 +0530

    Update program_array_05.java

--- Chapter_1/program_array_05.java
+++ Chapter_1/program_array_05.java
@@ -5,11 +5,11 @@ public class program_array_05 {
         //array declaration
         String frinends[] = new String[5];
         //inserting data in array by index
-        frinends[0]="Pravin";
-        frinends[1]="Pandurang";
-        frinends[2]="Tushar";
-        frinends[3]="Pranav";
-        frinends[4]="Umesh";
+        frinends[0]="komal";
+        frinends[1]="mahi";
+        frinends[2]="kamini";
+        frinends[3]="kiran";
+        frinends[4]="shweta";
         //display array elements
         for(String val:frinends){
             System.out.println(val);
