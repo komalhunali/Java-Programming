e2cc3d7ff487a11c107eee9b048607a23894b715
komal hunali <mahihunali@gmail.com>
2024-10-07 22:59:49 +0530

    Update program_if_05.java

--- Chapter_1/program_if_05.java
+++ Chapter_1/program_if_05.java
@@ -2,7 +2,7 @@ public class program_if_05 {
    public static void main(String args[]){
     boolean a = true;
     if(a = true){
-        System.out.print("Pravin is cleaver:"+a);
+        System.out.print("Komal is cleaver:"+a);
     }
    }  
 }
