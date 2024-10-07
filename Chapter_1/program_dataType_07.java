49cef9e950694c921e201fad6b8d0216a6e315ad
komal hunali <mahihunali@gmail.com>
2024-10-07 22:39:58 +0530

    Update program_dataType_07.java

--- Chapter_1/program_dataType_07.java
+++ Chapter_1/program_dataType_07.java
@@ -1,4 +1,4 @@
-public class Dt7 {
+public class program_dataType_07 {
     public static void main(String[] args) {
         long distance = 1234567890L;
         System.out.println("Distance: " + distance);
