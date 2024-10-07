3a1e3370be1afb2128656020f9f6e30d90f1fa38
komal hunali <mahihunali@gmail.com>
2024-10-07 23:23:25 +0530

    Update Program_java_09.java

--- Chapter_3/Program_java_09.java
+++ Chapter_3/Program_java_09.java
@@ -4,15 +4,15 @@ public class Program_java_09 {
     public static void main(String args[]){
         LinkedList list = new LinkedList<>();
         
-        list.add("Pravin");
-        list.add("pandurang");
-        list.add("Umesh");
-        list.add("pranav");
-        list.add("Tushar");
+        list.add("Komal");
+        list.add("Mahi");
+        list.add("Vaishnavi");
+        list.add("Maheshwari");
+        list.add("Vedika");
 
         System.out.println(list);
 
-        list.remove("Umesh"); //remove umesh from list
+        list.remove("varsha"); //remove varsha from list
         System.out.println(list);
 
         list.removeLast();  //remove last value from list
