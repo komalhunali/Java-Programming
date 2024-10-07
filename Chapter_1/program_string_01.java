8551f10ecfc39ed9b550c0e9dc80d8ca0fccce70
komal hunali <mahihunali@gmail.com>
2024-10-07 22:53:35 +0530

    Update program_string_01.java

--- Chapter_1/program_string_01.java
+++ Chapter_1/program_string_01.java
@@ -1,8 +1,8 @@
 public class program_string_01 {
     public static void main(String args[]){
         //string datatype variable
-        String firstName = "Pravin";
-        String lastName = "Junne";
+        String firstName = "Komal";
+        String lastName = "Hunali";
         //displaying variable
         System.out.println(firstName);
         System.out.println(lastName);
