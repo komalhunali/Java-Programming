db6a6494bee4815c59e6b59ec8bbcb48c5211b87
komal hunali <mahihunali@gmail.com>
2024-10-07 23:05:23 +0530

    Update program_method_03.java

--- Chapter_1/program_method_03.java
+++ Chapter_1/program_method_03.java
@@ -1,7 +1,7 @@
 public class program_method_03 {
     public static void main(String args[]){
         Main obj1 = new Main();
-        obj1.StudentInfo("KOMAL HUNALI", 4212);
+        obj1.StudentInfo("KOMAL HUNALI", 4268);
     }
     
 }
