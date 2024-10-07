7263107b91df5b92dcfe67b17bd3d75653107a3e
komal hunali <mahihunali@gmail.com>
2024-10-07 22:44:30 +0530

    Update program_03.java

--- Chapter_1/program_03.java
+++ Chapter_1/program_03.java
@@ -2,7 +2,7 @@
 import java.util.*;
 public class program_03 {
     public static void main(String args[]){
-        String name = "Pravin";
+        String name = "Komal";
         System.out.print(name+" Learning java programming.....");
     }
 }
