2335c6de4ebfc0ce72a71dcd5a0eac8e465b3244
komal hunali <mahihunali@gmail.com>
2024-10-07 22:52:20 +0530

    Update program_string_05.java

--- Chapter_1/program_string_05.java
+++ Chapter_1/program_string_05.java
@@ -1,7 +1,7 @@
 public class program_string_05 {
     public static void main(String args[]){
         //string declaration using new keyword
-        String name = new String("Pravin");
+        String name = new String("Komal");
         System.out.print(name);
     }
 }
