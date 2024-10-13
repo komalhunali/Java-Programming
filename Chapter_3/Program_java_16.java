29c08caa05aa6e207aea5d6beae7bd337ca50d7a
komal hunali <mahihunali@gmail.com>
2024-10-13 23:27:36 +0530

    Add files via upload

---
+++ Chapter_3/Program_java_16.java
@@ -0,0 +1,13 @@
+//LinkedHasSet
+import java.util.*;
+public class Program_java_16 {
+    public static void main(String args[]){
+        //set
+        Set<String> data = new LinkedHashSet<String>();
+        //adding elements
+        data.add("Komal");
+        data.add("Google");
+        //
+        System.out.println(data);
+    }
+}
